1a4f492925550c9938471b908bae9bb1b2f9a263
gabri-beep <biel790832157@gmail.com>
2025-03-17 23:11:00 -0300

    Ex10_StatusCode_Gabriel_Rodigues

---
+++ exercicio10_MVC_StatusCode/src/main/java/com/example/exercicio10_StatusCode/Exercicio10StatusCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercicio10_StatusCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio10StatusCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio10StatusCodeApplication.class, args);
+	}
+
+}
